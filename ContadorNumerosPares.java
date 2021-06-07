4cd908baabd0b859e482e216864dac543262396a
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-07 15:24:26 -0300

    Adicionando soma de matrizes

--- ContadorNumerosPares.java
+++ ContadorNumerosPares.java
@@ -33,35 +33,8 @@ public class ContadorNumerosPares {
 		media = (float) somaNumeros(numeros) / numeros.length;
 		return media; 
 	 }
-}
-
-
-/*public class ContadorNumerosPares {
-	int soma;
 	
-	public int contaNumerosPares(int[] numeros) {
-		int totNumPar = 0;
-		for(int i = 0; i < numeros.length; i++) {
-			boolean isPar = numeros[i]%2==0;
-			if(isPar) {
-				totNumPar++;
-			} 
-		} 
-		return totNumPar;	
-	}
-	
-	public void somaNumeros(int[] numeros) {
-
-		for(int i = 0; i < numeros.length; i++) {
-			soma = numeros[i] + soma;
-		}
-		System.out.println(soma);
-		mediaNumeros(numeros);
-	}
-	
-	 public void mediaNumeros(int[] numeros) {
-		 
-		 soma = soma / numeros.length;
-		 System.out.println(soma);
+	 public int somaMatrizNumeros(int[][] numeros){
+	    return 0;	 
 	 }
-}*/
+}
