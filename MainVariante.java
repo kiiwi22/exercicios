dcf7a7895043602a33a2f9b7be20076f6beecc95
Wandy <wandy@ilustrearte.com>
2021-06-07 14:32:37 -0300

    Main Somando e Dividindo

---
+++ MainVariante.java
@@ -0,0 +1,17 @@
+package main;
+
+import contador.ContadorNumerosPares;
+
+public class MainVariante {
+
+	public static void main(String[] args) {
+		ContadorNumerosPares c1 = new ContadorNumerosPares();
+		
+		int[] numeros = new int[]{1,2,3,4,5,6,7,8,9};
+		c1.somaNumeros(numeros);
+		
+	}
+
+}
+
+
