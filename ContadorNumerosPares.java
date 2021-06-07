528f3d270404ee07e4a68e8978e17ebed50390b7
Wandy <wandy@ilustrearte.com>
2021-06-07 15:13:14 -0300

    com while

--- ContadorNumerosPares.java
+++ ContadorNumerosPares.java
@@ -1,6 +1,42 @@
 package contador;
 
+
 public class ContadorNumerosPares {
+	
+	public int contaNumerosPares(int[] numeros) {
+		int totNumPar = 0;
+		int i = 0;
+		while( i < numeros.length) {
+			boolean isPar = numeros[i]%2==0;
+			if(isPar) {
+				totNumPar++;
+				i++;
+			}
+			
+		}
+		return totNumPar;	
+	}
+	
+	public int somaNumeros(int[] numeros) {
+		int soma = 0;
+		int i = 0;
+		while( i < numeros.length) {
+			soma += numeros[i];
+			i++;	
+		} System.out.println(soma);
+		
+		return soma;
+	}
+	
+	 public float mediaNumeros(int[] numeros) {
+		float media; 
+		media = (float) somaNumeros(numeros) / numeros.length;
+		return media; 
+	 }
+}
+
+
+/*public class ContadorNumerosPares {
 	int soma;
 	
 	public int contaNumerosPares(int[] numeros) {
@@ -28,4 +64,4 @@ public class ContadorNumerosPares {
 		 soma = soma / numeros.length;
 		 System.out.println(soma);
 	 }
-}
+}*/
