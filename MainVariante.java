97d482e161ae724ea84a5d058172ec3136cc82cc
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-07 15:24:31 -0300

    Adicionando soma de matrizes

--- MainVariante.java
+++ MainVariante.java
@@ -6,7 +6,8 @@ public class MainVariante {
 
 	public static void main(String[] args) {
 		ContadorNumerosPares c1 = new ContadorNumerosPares();
-		
+		int[][] matrizNumeros = new int[][]{{1,2,3,4,5},
+                                                    {6,7,8,9,10}};
 		int[] numeros = new int[]{1,2,3,4,5,6,7,8,9};
 		c1.somaNumeros(numeros);
 		c1.mediaNumeros(numeros);
