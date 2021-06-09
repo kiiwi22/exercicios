a9e1a07fcc7d201da3acd94ac5141ee20dd49f11
Wandy <wandy@ilustrearte.com>
2021-06-09 09:53:12 -0300

    Somando Matriz bidimensional

--- ContadorNumerosPares.java
+++ ContadorNumerosPares.java
@@ -33,8 +33,48 @@ public class ContadorNumerosPares {
 		media = (float) somaNumeros(numeros) / numeros.length;
 		return media; 
 	 }
-	
-	 public int somaMatrizNumeros(int[][] numeros){
-	    return 0;	 
+	 
+	 
+	 public int somaMatrizNumeros(int[][] numeros2){
+		 int soma = 0;
+		 for(int [] i : numeros2) {
+			 for(int j : i) {
+				 soma += j;
+			 }
+		 }
+		 
+	    return soma;	 
 	 }
+	 
 }
+
+
+/*public class ContadorNumerosPares {
+	int soma;
+	
+	public int contaNumerosPares(int[] numeros) {
+		int totNumPar = 0;
+		for(int i = 0; i < numeros.length; i++) {
+			boolean isPar = numeros[i]%2==0;
+			if(isPar) {
+				totNumPar++;
+			} 
+		} 
+		return totNumPar;	
+	}
+	
+	public void somaNumeros(int[] numeros) {
+
+		for(int i = 0; i < numeros.length; i++) {
+			soma = numeros[i] + soma;
+		}
+		System.out.println(soma);
+		mediaNumeros(numeros);
+	}
+	
+	 public void mediaNumeros(int[] numeros) {
+		 
+		 soma = soma / numeros.length;
+		 System.out.println(soma);
+	 }
+}*/
--- MainVariante.java
+++ MainVariante.java
@@ -6,11 +6,35 @@ public class MainVariante {
 
 	public static void main(String[] args) {
 		ContadorNumerosPares c1 = new ContadorNumerosPares();
-		int[][] matrizNumeros = new int[][]{{1,2,3,4,5},
-                                                    {6,7,8,9,10}};
-		int[] numeros = new int[]{1,2,3,4,5,6,7,8,9};
-		c1.somaNumeros(numeros);
-		c1.mediaNumeros(numeros);
+		int[][] numeros2 = new int [4][6];
+		numeros2[0][0] = 1;
+		numeros2[0][1] = 1;
+		numeros2[0][2] = 1;
+		numeros2[0][3] = 1;
+		numeros2[0][4] = 1;
+		numeros2[0][5] = 1;
+		numeros2[1][0] = 1;
+		numeros2[1][1] = 1;
+		numeros2[1][2] = 1;
+		numeros2[1][3] = 1;
+		numeros2[1][4] = 1;
+		numeros2[1][5] = 1;
+		numeros2[2][0] = 1;
+		numeros2[2][1] = 1;
+		numeros2[2][2] = 1;
+		numeros2[2][3] = 1;
+		numeros2[2][4] = 1;
+		numeros2[2][5] = 1;
+		numeros2[3][0] = 1;
+		numeros2[3][1] = 1;
+		numeros2[3][2] = 1;
+		numeros2[3][3] = 1;
+		numeros2[3][4] = 1;
+		numeros2[3][5] = 2;
+		
+		
+		System.out.println(c1.somaMatrizNumeros(numeros2));
+		
 	}
 
 }
