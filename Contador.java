34e842a9e19aa0b8430872873e3b2eeb67f26b02
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-10 13:06:53 -0300

    Troca de valores

--- Contador.java
+++ Contador.java
@@ -1,16 +1,10 @@
 
 public class Contador {
 	
-	public void trocaValores(int[]valor1) {
-		
-		int primeiraPosição = valor1[0];
-		int ultimaPosição = valor1[valor1.length-1];
-		int[]valor2 = {valor1.length-1};
-		valor2 = valor1;
-		System.out.println(valor2[0]+","+valor2[1]+","+valor2[2]+","+valor2[3]+","+valor2[4]);
-		valor2[0] = ultimaPosição;
-		valor2[valor2.length-1] = primeiraPosição;
-		System.out.println(valor2[0]+","+valor2[1]+","+valor2[2]+","+valor2[3]+","+valor2[4]);
+	public void trocaValores(int[] valores) {
+	   int aux = valores[0];
+           valores[0] = valores[valores.length-1];
+           valores[valores.length-1] = aux;
 	}
 	
 }
