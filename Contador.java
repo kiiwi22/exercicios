a1187bfb20c0142984798516fb6c52fc57cda5d6
Wandy <wandy@ilustrearte.com>
2021-06-11 13:24:53 -0300

    Soma e tira media Array

--- Contador.java
+++ Contador.java
@@ -1,16 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Contador {
-	
-	public void trocaValores(int[]valor1) {
 		
-		int primeiraPosição = valor1[0];
-		int ultimaPosição = valor1[valor1.length-1];
-		int[]valor2 = {valor1.length-1};
-		valor2 = valor1;
-		System.out.println(valor2[0]+","+valor2[1]+","+valor2[2]+","+valor2[3]+","+valor2[4]);
-		valor2[0] = ultimaPosição;
-		valor2[valor2.length-1] = primeiraPosição;
-		System.out.println(valor2[0]+","+valor2[1]+","+valor2[2]+","+valor2[3]+","+valor2[4]);
+	public int passaTamanhoArray() {
+		
+		Scanner valor = new Scanner (System.in);
+		int tamanhoArray;
+		System.out.println("Insira o tamanho do Array: ");
+		tamanhoArray = valor.nextInt();
+		
+		return tamanhoArray;
 	}
-	
+		
+	public int mediaValoresArray() {
+		
+		Scanner valor = new Scanner (System.in);
+		
+		int[] array1 = new int [passaTamanhoArray()];
+		
+		for(int i = 0; i <= array1.length-1;i++) {
+			System.out.print("Insira um valor para a posição " + i + " do Array.");
+			array1[i] = valor.nextInt();
+			
+		} System.out.println(Arrays.toString(array1));
+		
+		int soma = 0;
+		for( int i : array1) {
+		    soma += i;
+		} System.out.println("A soma dos valores do Array é: " + soma);
+		int mediaArray = soma / array1.length;
+		System.out.println("A media dos valores do Array é: " + mediaArray);
+		return mediaArray;
+		}
 }
+
+
