b6edbce55e0a2c982635df988818b235605c1ebf
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-18 14:03:08 -0300

    Refatoring

--- ProgressaoAritmetica.java
+++ ProgressaoAritmetica.java
@@ -7,19 +7,18 @@ public class ProgressaoAritmetica {
 		Scanner valor = new Scanner (System.in);
 		int tamanhoProgressao;
 		int constanteProgressao;
-		System.out.println("Digite o tamanho da progress„o: ");
+		System.out.println("Digite o tamanho da progress√£o: ");
 		tamanhoProgressao = valor.nextInt();
-		System.out.println("Digite a constante da progress„o: ");
+		System.out.println("Digite a constante da progress√£o: ");
 		constanteProgressao = valor.nextInt();
 		valor.close();
-		int constanteTemp = 0;
-		int constantePermanente = constanteProgressao;
-		for (int i = 0; i < tamanhoProgressao; i++) {	
-			constanteProgressao += constanteTemp;
-			constanteTemp = constantePermanente;
-			System.out.println(constanteProgressao);
-		}
-		return constanteProgressao;
+		
+		int constantePermanente = 0;
+       		for (int i = 0; i < tamanhoProgressao; i++) {
+            	    constantePermanente += constanteProgressao;
+                    System.out.println(constantePermanente);
+                }
+        	return constanteProgressao;
 	}
 	
 }
