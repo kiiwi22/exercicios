0772207e0b0658500289f897ab8663810679ee35
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-22 17:11:52 -0300

    Organizando código

--- CadastroPessoa.java
+++ CadastroPessoa.java
@@ -4,30 +4,27 @@ import java.util.Scanner;
 public class CadastroPessoa {
 
 	public Pessoa[] cadastraPessoa() {
-		Scanner valor = new Scanner (System.in);
-		String nome = "";
-		Integer idade = 0; 
-		Integer tamanho;
-		System.out.print("Quantas pessoas deseja cadastrar? ");
-		tamanho = valor.nextInt();
-		Pessoa[] nomePessoas = new Pessoa [tamanho];
-		
-		for(int i = 0; i <= nomePessoas.length-1;i++) {
-			nomePessoas[i] = new Pessoa(nome, idade);
-			System.out.println("nome: ");
-			nome = valor.next();
-			nomePessoas[i].setNome(nome);
-			System.out.println("idade: ");
-			idade = valor.nextInt();
-			nomePessoas[i].setIdade(idade);
-		}System.out.println(Arrays.toString(nomePessoas));
-		valor.close();
-		return nomePessoas;
-	}
+       	Scanner valor = new Scanner(System.in);
+        Integer tamanho;
+        System.out.print("Quantas pessoas deseja cadastrar? ");
+        tamanho = valor.nextInt();
+        Pessoa[] pessoas = new Pessoa[tamanho];
+
+        for(int i = 0; i < pessoas.length;i++) {
+            pessoas[i] = new Pessoa();
+            System.out.println("nome: ");
+            pessoas[i].setNome(valor.next());
+            System.out.println("idade: ");
+            pessoas[i].setIdade(valor.nextInt());
+        }
+        System.out.println(Arrays.toString(pessoas));
+        valor.close();
+        return pessoas;
+    }
 }
 
 /*vai criar uma classe Pessoa com os campos nome e idade
 vai criar um array com alguns objetos Pessoa e vai fazer uma busca por parte do nome da pessoa
-isso vai ser informado pelo usuário e vai apresentar todas as pessoas que entrarem no filtro
-nome final apresentar a menor e a maior idade se tu quiser deixar dinâmico pode solicitar a quantidade
-de registros pro usuário e informar o nome e a idade de cada pessoa e depois apresenta essas informações*/
+isso vai ser informado pelo usuÃ¡rio e vai apresentar todas as pessoas que entrarem no filtro
+nome final apresentar a menor e a maior idade se tu quiser deixar dinÃ¢mico pode solicitar a quantidade
+de registros pro usuÃ¡rio e informar o nome e a idade de cada pessoa e depois apresenta essas informaÃ§Ãµes*/
