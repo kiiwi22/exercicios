8a866546c1c6662def85e42bda0ba027253f9a60
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-06-22 13:22:43 -0300

    Ajustando algoritmo

--- AchaNumeros.java
+++ AchaNumeros.java
@@ -2,31 +2,23 @@
 public class AchaNumeros {
 
 	public int encontraMenor(int[] numeros) {
-		int maior = 0;
-
-		for (int numero : numeros) {
-			if (numero > maior) {
-				maior = numero;
-			}
-		}
-		int menor = maior;
-
-		for (int numeroArray : numeros) {
-			if (numeroArray < menor) {
-				menor = numeroArray;
-			}
-		}
-		return menor;
+		int menor = Integer.MAX_VALUE;
+        for (int numero : numeros) {
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+        return menor;
 	}
 }
 /*
- * 1 - método recebe um parámetro de array 2 - cria uma variavel para receber o
- * valor do retorno (maior = 0;), no caso do encontraMaior() é retornar o maior
+ * 1 - mÃ©todo recebe um parÃ¡metro de array 2 - cria uma variavel para receber o
+ * valor do retorno (maior = 0;), no caso do encontraMaior() Ã© retornar o maior
  * int do array 3 - inicia o foreach, numero : numeros (a variavel temporaria
  * numero recebe os valores do array numeros) 4 - inicia um if, compara numero
- * que contém o valor da posição atual x do array numeros com a variavel criada
- * anteriormente. Caso o valor da posição atual seja maior que o valor da
- * variável "maior" então a variavel recebe o valor da posição x. 5 - no final
- * do if a variavel "maior" terá o mesmo valor que a posição x do array com o
+ * que contÃ©m o valor da posiÃ§Ã£o atual x do array numeros com a variavel criada
+ * anteriormente. Caso o valor da posiÃ§Ã£o atual seja maior que o valor da
+ * variÃ¡vel "maior" entÃ£o a variavel recebe o valor da posiÃ§Ã£o x. 5 - no final
+ * do if a variavel "maior" terÃ¡ o mesmo valor que a posiÃ§Ã£o x do array com o
  * maior valor 6 - retorna a variavel "maior" com o maior valor do array
  */
