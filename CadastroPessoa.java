2752f0f02a77d12145735c23be0592f7d3d8d5c2
Wandy <wandy@ilustrearte.com>
2021-06-23 13:53:42 -0300

    refatored final by wandy

--- CadastroPessoa.java
+++ CadastroPessoa.java
@@ -3,41 +3,81 @@ import java.util.Scanner;
 
 public class CadastroPessoa {
 
-	public Pessoa[] cadastraPessoa() {
+	public PessoaA[] cadastraPessoa() {
 		Scanner valor = new Scanner(System.in);
 		Integer tamanho;
 		System.out.print("Quantas pessoas deseja cadastrar? ");
 		tamanho = valor.nextInt();
-		Pessoa[] cadastraPessoas = new Pessoa[tamanho];
+		PessoaA[] cadastraPessoas = new PessoaA[tamanho];
 
 		for (int i = 0; i < cadastraPessoas.length; i++) {
-			cadastraPessoas[i] = new Pessoa();
+			cadastraPessoas[i] = new PessoaA();
 			System.out.println("nome: ");
 			cadastraPessoas[i].setNome(valor.next());
 			System.out.println("idade: ");
 			cadastraPessoas[i].setIdade(valor.nextInt());
 		}
 		System.out.println(Arrays.toString(cadastraPessoas));
-		valor.close();
+
 		return cadastraPessoas;
 	}
 
-	public String[] buscaPessoa(Pessoa[] cadastraPessoas) {
+	public String[] buscaPessoa(PessoaA[] cadastraPessoas) {
 		String busca;
 		String[] resultadoBusca = new String[cadastraPessoas.length];
 		Scanner valorBusca = new Scanner(System.in);
 		System.out.println("Qual pesquisa deseja fazer: ");
 		busca = valorBusca.next();
 		for (int i = 0; i < cadastraPessoas.length; i++) {
-			if (cadastraPessoas[i].getNome().contains(busca)) {
+			if (cadastraPessoas[i].getNome().toLowerCase().contains(busca.toLowerCase())) {
 				System.out.println(cadastraPessoas[i].getNome());
+				resultadoBusca[i] = cadastraPessoas[i].getNome();
 			}
 		}
 		System.out.println(Arrays.toString(resultadoBusca));
-		valorBusca.close();
+
 		return resultadoBusca;
 	}
 }
+
+class PessoaA {
+	private String nome;
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+	}
+
+}
+
+/*
+ * MAIN PARA RODAR O MÉTODO DE CADASTRO SEPARADO DO MÉTODO DE BUSCA 
+ * 
+ * public class Main {
+ * public static void main(String[] args) {
+ * CadastroPessoa c1 = new CadastroPessoa(); 
+ * Pessoa[] arrayPessoas = c1.cadastraPessoa();
+ * c1.buscaPessoa(arrayPessoas); 
+ * 		}
+ *	}
+ */
 
 /*
  * vai criar uma classe Pessoa com os campos nome e idade vai criar um array com
