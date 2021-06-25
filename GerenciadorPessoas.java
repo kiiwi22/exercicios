fb4541faabb7c011448895bb449ab4932aef6e9e
Wandy <wandy@ilustrearte.com>
2021-06-25 14:43:59 -0300

    new

---
+++ GerenciadorPessoas.java
@@ -0,0 +1,115 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GerenciadorPessoas {
+	
+	public Pessoa[] cadastraPessoa() {
+		Scanner valor = new Scanner(System.in);
+		Integer tamanho;
+		System.out.print("Quantas pessoas deseja cadastrar? ");
+		tamanho = valor.nextInt();
+		Pessoa[] cadastraPessoas = new Pessoa[tamanho];
+
+		for (int i = 0; i < cadastraPessoas.length; i++) {
+			cadastraPessoas[i] = new Pessoa();
+			System.out.println("nome: ");
+			cadastraPessoas[i].setNome(valor.next());
+			System.out.println("idade: ");
+			cadastraPessoas[i].setIdade(valor.nextInt());
+		}
+		System.out.println(Arrays.toString(cadastraPessoas));
+
+		return cadastraPessoas;
+	}
+	
+	public String[] buscaPessoa(Pessoa[] cadastraPessoas) {
+		String busca;
+		String[] resultadoBusca = new String[cadastraPessoas.length];
+		Scanner valorBusca = new Scanner(System.in);
+		System.out.println("Qual pesquisa deseja fazer: ");
+		busca = valorBusca.next();
+		for (int i = 0; i < cadastraPessoas.length; i++) {
+			if (cadastraPessoas[i].getNome().toLowerCase().contains(busca.toLowerCase())) {
+				System.out.println(cadastraPessoas[i].getNome());
+				resultadoBusca[i] = cadastraPessoas[i].getNome();
+			}
+		}
+		System.out.println(Arrays.toString(resultadoBusca));
+
+		return resultadoBusca;
+	}
+	
+	public int achaMaiorIdade (Pessoa[] cadastraPessoas) {
+		
+		int maiorIdade = 0;
+		
+		for (int i = 0; i < cadastraPessoas.length; i++) {
+			if(cadastraPessoas[i].getIdade() > maiorIdade) {
+				maiorIdade = cadastraPessoas[i].getIdade();
+			}
+		}
+		/*System.out.println("A maior idade é: "+maiorIdade);*/
+		return maiorIdade;
+	}
+	
+	public int achaMenorIdade (Pessoa[] cadastraPessoas) {
+		int menorIdade = 10000;
+		
+		for (int i = 0; i < cadastraPessoas.length; i++) {
+			if(cadastraPessoas[i].getIdade() < menorIdade) {
+				menorIdade = cadastraPessoas[i].getIdade();
+			}
+		}
+		/*System.out.println("A menor idade é: " + menorIdade);*/
+		return menorIdade;
+	}
+	
+	public String achaMaiorNomeRelacionadoIdade (Pessoa[] cadastraPessoas) {
+		String maiorNomeRelacionadoIdade = "";
+		
+		for(Pessoa array : cadastraPessoas) {
+			if(array.getIdade() == achaMaiorIdade(cadastraPessoas)) {
+				maiorNomeRelacionadoIdade = array.getNome();
+			}
+		}
+		System.out.println("O usuário mais velho cadastrado é: " + maiorNomeRelacionadoIdade + " e a idade dele é: " + achaMaiorIdade(cadastraPessoas) + " anos.");
+		return maiorNomeRelacionadoIdade;
+	}
+	
+	public String achaMenorNomeRelacionadoIdade (Pessoa[] cadastraPessoas) {
+		String menorNomeRelacionadoIdade = "";
+		
+		for(Pessoa array : cadastraPessoas) {
+			if(array.getIdade() == achaMenorIdade(cadastraPessoas)) {
+				menorNomeRelacionadoIdade = array.getNome();
+			}
+		}
+		System.out.println("O usuário mais novo cadastrado é: "+menorNomeRelacionadoIdade + " e a idade dele é: " + achaMenorIdade(cadastraPessoas) + " anos.");
+		return menorNomeRelacionadoIdade;
+	}
+	
+	/*public Pessoa[] achaMaiorNomeIdade (Pessoa[] cadastraPessoas) {
+		
+		int tamanho = 0;
+		
+		for(Pessoa array : cadastraPessoas) {
+			if(array.getIdade() == achaMaiorIdade(cadastraPessoas)) {
+				tamanho++;
+			}if(array.getIdade() == achaMenorIdade(cadastraPessoas)) {
+				tamanho++;
+			}
+		}
+		Pessoa[] arrayAchaMaiorNomeIdade = new Pessoa[tamanho];
+		for(int i = 0; i < cadastraPessoas.length; i++) {
+			if(cadastraPessoas[i].getIdade() == achaMaiorIdade(cadastraPessoas)) {
+				arrayAchaMaiorNomeIdade[i].setIdade(achaMaiorIdade(cadastraPessoas));
+				arrayAchaMaiorNomeIdade[i].setNome(achaNomeMaiorIdade(cadastraPessoas));
+			}if(cadastraPessoas[i].getIdade() == achaMenorIdade(cadastraPessoas)) {
+				arrayAchaMaiorNomeIdade[i].setIdade(achaMenorIdade(cadastraPessoas));
+				arrayAchaMaiorNomeIdade[i].setNome(achaNomeMenorIdade(cadastraPessoas));
+			}
+		}
+		System.out.println("Resultado Final: " + Arrays.toString(arrayAchaMaiorNomeIdade));
+		return arrayAchaMaiorNomeIdade;
+	}*/
+}
