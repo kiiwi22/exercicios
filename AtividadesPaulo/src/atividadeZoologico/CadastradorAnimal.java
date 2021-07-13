fb7746fac2e844f42cb24d943e73e2d728722be5
pauloribeiro-it <pauloroberto.ar14@gmail.com>
2021-07-13 19:54:02 -0300

    Update CadastradorAnimal.java
    Ajustes

--- AtividadesPaulo/src/atividadeZoologico/CadastradorAnimal.java
+++ AtividadesPaulo/src/atividadeZoologico/CadastradorAnimal.java
@@ -1,54 +1,47 @@
-package atividadeZoologico;
+package zoologico;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class CadastradorAnimal extends Animal{
 
-	public int quantidadeAnimaisCadastrados() {
-		
-		Scanner valor = new Scanner (System.in);
-		int quantidadeAnimais = 0;
-		System.out.println("Quantos animais deseja cadastrar?");
-		quantidadeAnimais = valor.nextInt();
-		
-		
-		return quantidadeAnimais;
-	}
-	
-	public ArrayList<Animal> cadastrandoAnimais(){
-		
-		ArrayList<Animal> animais = new ArrayList<Animal>(quantidadeAnimaisCadastrados());
-		Scanner valor = new Scanner (System.in);
-		
-		Animal[] animal = new Animal[quantidadeAnimaisCadastrados()];
-		int especie;
-		String nome;
-		Integer idade;
-		Double peso;
-		for(int i = 0; i <= quantidadeAnimaisCadastrados(); i++) {
-			animal[i] = new Animal();
-			System.out.println("Qual o nome do animal?");	
-			nome = valor.next();
-			animal[i].setNome(nome);
-			System.out.println("Qual a idade do animal?");
-			idade = valor.nextInt();
-			animal[i].setIdade(idade);
-			System.out.println("Qual o peso do animal?");
-			peso = valor.nextDouble();
-			animal[i].setPeso(peso);
-			System.out.println("Qual a espécie do animal? Digite 1 - Leão, 2 - Elefante, ou 3 - Macaco");
-			especie = valor.nextInt();
-				switch(especie) {
-				case 1:animal[i].setEspecieAnimal(getEspecieAnimal().Leão);
-				case 2:animal[i].setEspecieAnimal(getEspecieAnimal().Elefante);
-				case 3:animal[i].setEspecieAnimal(getEspecieAnimal().Macaco);
-				default:
-					System.out.println("Código inválido.");
-				}
-			animais.add(animal[i]);
-		}	
-		return animais;
-	}
+    private final Leitor leitor = new Leitor();
+
+    public int quantidadeAnimaisCadastrados() {
+        return leitor.leInteiro("Quantos animais deseja cadastrar?");
+    }
+
+    public ArrayList<Animal> cadastrandoAnimais(){
+        int qtdAnimais = quantidadeAnimaisCadastrados();
+
+        ArrayList<Animal> animais = new ArrayList<Animal>(qtdAnimais);
+
+        Animal[] animal = new Animal[qtdAnimais];
+        int especie;
+        String nome;
+        Integer idade;
+        Double peso;
+
+        for(int i = 0; i < qtdAnimais; i++) {
+            animal[i] = new Animal();
+            nome = leitor.leString("Qual o nome do animal?");
+            animal[i].setNome(nome);
+            idade = leitor.leInteiro("Qual a idade do animal?");
+            animal[i].setIdade(idade);
+            peso = leitor.leDouble("Qual o peso do animal?");
+            animal[i].setPeso(peso);
+            especie = leitor.leInteiro("Qual a espÃ©cie do animal? Digite 1 - LeÃ£o, 2 - Elefante, ou 3 - Macaco");
+            switch(especie) {
+                case 1:animal[i].setEspecieAnimal(getEspecieAnimal().LeÃ£o);
+                break;
+                case 2:animal[i].setEspecieAnimal(getEspecieAnimal().Elefante);
+                break;
+                case 3:animal[i].setEspecieAnimal(getEspecieAnimal().Macaco);
+                break;
+                default:
+                    System.out.println("CÃ³digo invÃ¡lido.");
+            }
+            animais.add(animal[i]);
+        }
+        return animais;
+    }
 }
