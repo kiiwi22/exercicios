bc49dd3999f1551be3f0ce2a591d56517ca59396
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2022-06-15 20:51:54 -0300

    Adicionando projeto

---
+++ carros/src/main/java/br/com/carros/entity/Pessoa.java
@@ -0,0 +1,77 @@
+package br.com.carros.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="data_nascimento")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	@Column(name="cpf")
+	private String cpf;
+	
+	@Column(name="sexo")
+	private String sexo;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(java.sql.Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+}
---
+++ carros/src/main/java/br/com/carros/mb/CarrosMB.java
@@ -0,0 +1,10 @@
+package br.com.carros.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CarrosMB {
+	
+}
